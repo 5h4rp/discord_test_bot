1e70a452a2cbe4ff5538e07bfc1608d7514f5da3
ajayender <ajayender@live.in>
2021-07-19 04:10:10 +0530

    basic bot stuff

--- test_bot.py
+++ test_bot.py
@@ -1,8 +1,11 @@
 # bot.py
 import os
+import random
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -10,24 +13,82 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 
 intents = discord.Intents.all()
-
 activity = discord.Activity(type=discord.ActivityType.playing, name='with code')
-client = discord.Client(intents=intents, activity=activity)
+bot = commands.Bot(command_prefix='!', intents=intents, activity=activity)
 
-@client.event
+
+@bot.event
 async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
     # guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
-    guild = discord.utils.get(client.guilds, name=GUILD)
+    guild = discord.utils.get(bot.guilds, name=GUILD)
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+    # print(
+    #     f'{client.user} is connected to the following guild:\n'
+    #     f'{guild.name}(id: {guild.id})'
+    # )
 
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
-    owner = guild.owner
-    print(owner)
+    # owner = guild.owner
+    # print(owner)
 
-client.run(TOKEN)
+
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+
+@bot.command(name='99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int = 1, number_of_sides: int = 6):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+
+@bot.command(name='create-channel')
+@commands.has_role('test-role')
+async def create_channel(ctx, channel_name='test-channel'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+
+# @bot.event
+# async def on_error(event, *args, **kwargs):
+#     with open('err.log', 'a') as f:
+#         if event == 'on_message':
+#             f.write(f'Unhandled message: {args[0]}\n')
+#         else:
+#             raise
+
+bot.run(TOKEN)
